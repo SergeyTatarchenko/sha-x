e1edd90e9aa70000617ee43f89423b6c9d4a2259
SergeyTatarchenko <zlojdigger@gmail.com>
2020-03-17 23:01:25 +0300

    -fix hash output for 0x0XXXXXXX

--- main.cpp
+++ main.cpp
@@ -14,8 +14,8 @@ int main(int argc, char *argv[])
     /*the number of lines and characters in the file*/
     int NumOfSymbols = 0,counter = 0;
     sha_1 SHA_1; /*checksum instance */
-
-    if(argc = 2)
+    char buff = 0;
+    if(argc == 3)
         {
             /*open stream for reading hash file*/
             FileReader.open(argv[1]);
@@ -26,12 +26,15 @@ int main(int argc, char *argv[])
                 }
             do
                 {
-                    FileReader.get();
+                    buff = FileReader.get();
+                    if((buff == 0x0A)||(buff == -1)||(buff == 0x0D))
+                    {
+                         continue;
+                    }
                     NumOfSymbols++;
                 }
             while(!FileReader.eof());
             FileReader.close();
-            NumOfSymbols--; /*subtract EOF character*/
 
             SHA_1.GetNumberOfBlocks(NumOfSymbols);
             SHA_1.MemPointer = new unsigned char [SHA_1.NumOfBlocks * SHA_1.BlockSize];
@@ -50,10 +53,30 @@ int main(int argc, char *argv[])
             SHA_1.CaclChecksum();
             FileWritter.open(argv[2]);
             FileWritter<<"Calculated checksum :"<<endl;
+            if((SHA_1.HA&(uint32_t)0xF0000000) == 0)
+            {
+                FileWritter<<'0';
+            }
             FileWritter<<hex<<SHA_1.HA;
+            if((SHA_1.HB&(uint32_t)0xF0000000) == 0)
+            {
+                FileWritter<<'0';
+            }
             FileWritter<<hex<<SHA_1.HB;
+            if((SHA_1.HC&(uint32_t)0xF0000000) == 0)
+            {
+                FileWritter<<'0';
+            }
             FileWritter<<hex<<SHA_1.HC;
+            if((SHA_1.HD&(uint32_t)0xF0000000) == 0)
+            {
+                FileWritter<<'0';
+            }
             FileWritter<<hex<<SHA_1.HD;
+            if((SHA_1.HE&(uint32_t)0xF0000000) == 0)
+            {
+                FileWritter<<'0';
+            }
             FileWritter<<hex<<SHA_1.HE;
 
             FileWritter.close();
